4f6cf7d19af60341f4e6a6408566f807f1bd38cc
Andrea <andysofyhc14@gmail.com>
2017-04-20 15:21:02 -0600

    holii3

--- sv.edu.uesocc.casosacad.project/sv.edu.uesocc.casosacad.project-web/src/main/java/managedBeans/mbPaso.java
+++ sv.edu.uesocc.casosacad.project/sv.edu.uesocc.casosacad.project-web/src/main/java/managedBeans/mbPaso.java
@@ -24,7 +24,6 @@ import sv.edu.uesocc.casosacad.pojos.PasoFacadeLocal;
  * Gestion de Casos, Administracion Academica
  *
  */
-
 @Named(value = "mbPaso")
 @ViewScoped
 public class mbPaso implements Serializable {
